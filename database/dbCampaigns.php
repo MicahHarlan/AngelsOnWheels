da206f9a3ea5e2daeed1ec701621b1693e23207e
MicahHarlan <81989762+MicahHarlan@users.noreply.github.com>
2023-03-08 13:29:47 -0500

    db_campaigns
    db campaigns initialized first push

---
+++ database/dbCampaigns.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * Copyright 2013 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook, 
+ * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan, 
+ * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker. 
+ * This program is part of RMH Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
+ * 
+ */
+
+/**
+ * @version March 1, 2012
+ * @author Oliver Radwan and Allen Tucker
+ */
+
+/* 
+ * Created for Gwyneth's Gift in 2022 using original Homebase code as a guide
+ */
+
+
+include_once('dbinfo.php');
+include_once(dirname(__FILE__).'/../domain/Campaign.php');
+
+/*
+ * add an event to dbEvents table: if already there, return false
+ */
+
+function add_campaign($campaign) {
+    if (!$campaign instanceof Campaign)
+        die("Error: add_campaign type mismatch");
+    $con=connect();
+    $query = "SELECT * FROM dbCampaigns WHERE id = '" . $campaign->get_campaign_id() . "'";
+    $result = mysqli_query($con,$query);
+    //if there's no entry for this id, add it
+    if ($result == null || mysqli_num_rows($result) == 0) {
+        mysqli_query($con,'INSERT INTO dbCampaigns VALUES("' .              
+               // $campaign->get_event_date() . '","' .
+              //  $campaign->get_venue() . '","' .
+                $campaign->get_campaign_name() . '","' . 
+                $campaign->get_description() . '","' .
+                $campaign->get_campaign_id() .            
+                '");');							
+        mysqli_close($con);
+        return true;
+    }
+    mysqli_close($con);
+    return false;
+}
+
+/*
+ * remove an event from dbEvents table.  If already there, return false
+ */
+
+function remove_campaign($id) {
+    $con=connect();
+    $query = 'SELECT * FROM dbEvents WHERE id = "' . $id . '"';
+    $result = mysqli_query($con,$query);
+    if ($result == null || mysqli_num_rows($result) == 0) {
+        mysqli_close($con);
+        return false;
+    }
+    $query = 'DELETE FROM dbEvents WHERE id = "' . $id . '"';
+    $result = mysqli_query($con,$query);
+    mysqli_close($con);
+    return true;
+}
+
+/*
+ * @return an Event from dbEvents table matching a particular id.
+ * if not in table, return false
+ */
+function retrieve_campaign($id) {
+    $con=connect();
+    $query = "SELECT * FROM dbCampaigns WHERE id = '" . $id . "'";
+    $result = mysqli_query($con,$query);
+    if (mysqli_num_rows($result) !== 1) {
+        mysqli_close($con);
+        return false;
+    }
+    $result_row = mysqli_fetch_assoc($result);
+    // var_dump($result_row);
+    $theCampaign = make_a_campaign($result_row);
+//    mysqli_close($con);
+    return $theCampaign;
+}
+
+
+
+//COULD BE USED FOR A DATE RANGE
+// not in use, may be useful for future iterations in changing how events are edited (i.e. change the remove and create new event process)
+function update_campaign_date($id, $new_event_date) {
+	$con=connect();
+	$query = 'UPDATE dbEvents SET event_date = "' . $new_event_date . '" WHERE id = "' . $id . '"';
+	$result = mysqli_query($con,$query);
+	mysqli_close($con);
+	return $result;
+}
+
+function make_a_campaign($result_row) {
+	/*
+	 ($en, $v, $sd, $description, $ev))
+	 */
+    $theCampaign = new Campaign(
+                    $result_row['campaign_name'],                                      
+                    $result_row['description'],
+                    $result_row['campaign_id']);  
+    return $theCampaign;
+}
+
+
+// retrieve only those events that match the criteria given in the arguments
+/* function getonlythose_dbCampaigns($name, $day, $venue) {
+   $con=connect();
+   $query = "SELECT * FROM dbEvents WHERE event_name LIKE '%" . $campaign_name . "%'" .
+           " AND event_name LIKE '%" . $name . "%'" .
+           " AND venue = '" . $venue . "'" . 
+           " ORDER BY event_name";
+   $result = mysqli_query($con,$query);
+   $theEvents = array();
+   while ($result_row = mysqli_fetch_assoc($result)) {
+       $theEvent = make_an_event($result_row);
+       $theEvents[] = $theEvent;
+   }
+   mysqli_close($con);
+   return $theEvents;
+} */
+
+?>
---
+++ domain/Campaign.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright 2013 by Allen Tucker. 
+ * This program is part of RMHC-Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
+ * 
+ */
+
+/*
+ * Created on Mar 28, 2008
+ * @author Oliver Radwan <oradwan@bowdoin.edu>, Sam Roberts, Allen Tucker
+ * @version 3/28/2008, revised 7/1/2015
+ */
+
+/* 
+ * Created for Gwyneth's Gift in 2022 using original Homebase code as a guide
+ */
+
+ class Campaign {
+	private $campaign_name;  // campaign name as a string
+	private $description;   // description of the campaign
+	private $campaign_id;		// the unique id that is attached to each campaign, is then copied into id, used for editing campaign
+
+
+	function __construct($en ,$description, $ev) {
+		$this->campaign_name = $en;
+		$this->description = $description;
+		$this->campaign_id = $ev;
+		
+	}
+
+	function get_campaign_name() {
+		return $this->campaign_name;
+	}
+
+	function get_description() {
+		return $this->description;
+	}
+
+	function get_campaign_id() {
+		return $this->campaign_id;
+	}
+
+}
+?>
