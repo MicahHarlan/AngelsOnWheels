a4caf5ef7549d3529cc4c51a42cdae0fa1e8c638
MicahHarlan <81989762+MicahHarlan@users.noreply.github.com>
2023-03-13 12:22:50 -0400

    Fixed DB
    Fixed

--- sql/homebasedb.sql
+++ sql/homebasedb.sql
@@ -22,6 +22,73 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 13, 2023 at 05:15 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `homebasedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dbCampaigns`
+--
+
+CREATE TABLE `dbCampaigns` (
+  `campaign_id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `campaign_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dbCampaigns`
+--
+
+INSERT INTO `dbCampaigns` (`campaign_id`, `description`, `campaign_name`) VALUES
+(12, 'Campaign description!! ', 'Campaign'),
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dbCampaigns`
+--
+ALTER TABLE `dbCampaigns`
+  ADD PRIMARY KEY (`campaign_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dbCampaigns`
+--
+ALTER TABLE `dbCampaigns`
+  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 --
 -- Table structure for table `dbCampaigns`
@@ -1169,73 +1236,6 @@ ALTER TABLE `dbweeks`
 --
 ALTER TABLE `dblog`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Mar 13, 2023 at 05:15 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `homebasedb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dbCampaigns`
---
-
-CREATE TABLE `dbCampaigns` (
-  `campaign_id` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `campaign_name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `dbCampaigns`
---
-
-INSERT INTO `dbCampaigns` (`campaign_id`, `description`, `campaign_name`) VALUES
-(12, 'Campaign description!! ', 'Campaign'),
-
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `dbCampaigns`
---
-ALTER TABLE `dbCampaigns`
-  ADD PRIMARY KEY (`campaign_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `dbCampaigns`
---
-ALTER TABLE `dbCampaigns`
-  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
