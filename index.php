10179dd0246b5e7d957988a260f6157cd6a48851
jmundie5 <jmundie5@gmail.com>
2023-03-15 13:26:06 -0400

    Added Helper Function for Future Dates and Future Event Notifications on the Home Page

--- index.php
+++ index.php
@@ -234,11 +234,29 @@ session_start();
                             </thead>
                             <tbody>
                             ');
-                        $events = get_future_events();
-                        foreach ($events as $event) {
-                            echo ('<tr><td class="searchResults">' . $event[1] . '</td>' .
-                                '<td class="searchResults">' . $event[2] . '</td></tr>');
+                        $con = connect();   
+                        $query = "SELECT * FROM dbevents";
+                        $result = mysqli_query($con, $query);
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            // Access the row's values using associative array syntax
+                            //echo $row['event_name'];
+                            
+                            $futureCheck = fix_date($row['event_date']);
+                            if ($futureCheck){
+                                echo ('<tr><td class="searchResults">' .$row['event_date']. '</td>' . 
+                                '<td class="searchResults">' . $row['event_name'] . '</td></tr>');
+                            }
+                            
+                            //echo ('<tr><td class="searchResults">' .$row['event_date']. '</td>' . 
+                              //  '<td class="searchResults">' . $row['event_name'] . '</td></tr>');
                         }
+                        echo ('</tbody></table><br>');
+
+                        //foreach ($events as $event) {
+                        //    
+                        //    echo ('<tr><td class="searchResults">' . $event[0] . '</td>' .
+                        //        '<td class="searchResults">' . $event[0] . '</td></tr>');
+                        //}
 
 
                     }
