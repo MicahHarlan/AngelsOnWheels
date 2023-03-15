5cd04e57354169c2f640d4aee58e11d653a21fab
Ryan Phillips <82561611+RyanPhillips-CPSC@users.noreply.github.com>
2023-03-15 02:05:06 -0400

    Fixed footer not being fixed on bottom and designed the login screen to look good on mobile

--- about.php
+++ about.php
@@ -10,109 +10,71 @@
  * 
  */
 
-	session_start();
-	session_cache_expire(30);
+session_start();
+session_cache_expire(30);
 ?>
-<html>
-	<head>
-		<title>
-			About
-		</title>
-		<link rel="stylesheet" href="lib\bootstrap\css\bootstrap.css" type="text/css" />
-	</head>
-	<body>
-		<div class="container-fluid">
-			<?PHP include('header.php');?>
-			<div id="content">
-				<p/>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    <link rel="stylesheet" href="lib\bootstrap\css\bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="styling/about.css" type="text/css" />
+</head>
+<body>
+<div class="container" style="padding-bottom: 100px;">
+    <?php include('header.php'); ?>
+    <div id="content" class="mt-4">
+        <h2 class="text-center"><strong>About Gwyneth's Gift Foundation</strong></h2>
+        <p class="text-center">Gwyneth’s Gift Foundation is devoted to making a difference in the community and the lives of those within. We hope that through our work,
+            we can serve as a catalyst for increasing the survival rate of those suffering from an out-of-hospital cardiac arrest because they received
+            immediate life-saving measures from a member within their community.</p>
 
-                				<h2><strong><center>About Gwyneth's Gift Foundation</center></strong></h2>
+        <p class="text-center">Gwyneth’s Gift Foundation is a 501(c)(3) tax-exempt organization. Gwyneth’s Gift Foundation is Guidestar Gold Transparency, a member of
+            the Rappahannock United Way Local Government Campaign (LGC) and the Commonwealth of Virginia Campaign (CVC).</p>
 
-                				<p class="text-center">Gwyneth’s Gift Foundation is devoted to making a difference in the community and the lives of those within. We hope that through our work,
-                				we can serve as a catalyst for increasing the survival rate of those suffering from an out-of-hospital cardiac arrest because they received
-                				immediate life-saving measures from a member within their community.</p>
+        <p class="text-center">Gwyneth’s Gift Foundation was founded in 2015 by Joel and Jennifer Griffin in honor of their oldest daughter, Gwyneth. Through the Foundation’s
+            work, Gwyneth’s spirit of caring, compassion, and community lives on as we create a world where everyone can save a life.</p>
 
-                				<p class="text-center">Gwyneth’s Gift Foundation is a 501(c)(3) tax-exempt organization. Gwyneth’s Gift Foundation is Guidestar Gold Transparency, a member of
-                				the Rappahannock United Way Local Government Campaign (LGC) and the Commonwealth of Virginia Campaign (CVC).
+        <div>
+            <h3 class="section-title text-center">Our Vision</h3>
+            <p class="text-center">To create a Culture of Action where communities are educated, confident, and empowered to save the lives of individuals suffering from cardiac arrest.</p>
+        </div>
+        <div>
+            <h3 class="section-title text-center">Our Mission</h3>
+            <p class="text-center">To raise awareness of Cardiopulmonary Resuscitation (CPR) and the use of Automated External Defibrillators (AEDs).</p>
+        </div>
 
-                				<p class="text-center">Gwyneth’s Gift Foundation was founded in 2015 by Joel and Jennifer Griffin in honor of their oldest daughter, Gwyneth. Through the Foundation’s
-                				work, Gwyneth’s spirit of caring, compassion, and community lives on as we create a world where everyone can save a life.
-                				<p>
-
-
-                				<table>
-                                    <tr>
-                                        <td>
-                                            <p style="font-size :100px;"><b><center>Our Vision</center></b></p>
-                                            <p><i><center/>To create a Culture of Action where communities are educated, confident and empowered to save the lives of individuals suffering from cardiac arrest.
-
-                                        </i></p>
-                                        </td>
-                                        <td>
-                                            <p><b><center>Our Mission</center></b></p>
-                                            <p><i/><center/>To raise awareness of Cardiopulmonary Resuscitation (CPR) and the use of Automated External Defibrillators (AEDs).
-                                        </p>
-                                        </td>
-                                    </tr>
-                                </table>
-
-                                <p style='font-size:100px;'><b><center>Our Team</center></b>
-                                </p>
-                                <table>
-                                    <tr>
-                                        <td></td><td><center>Jennifer Griffin</center>
-                                        </td><td></td><td></td><td><center>Joel Griffin</center></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Co-founder & President</td>
-                                        <td></td><td></td><td style='width:200px;'>&nbsp;&nbsp;&nbsp;Co-founder & Chairman</td>
-                                    </tr>
-                                    <tr><td></td></tr>
-                                    <tr><td></td></tr>
-                                    <tr><td></td></tr>
-                                    <tr><td></td></tr>
-                                    <tr>
-                                        <td style="width:150px"></td>
-                                        <td>
-                                        <BR> <BR> <a href="tutorial/screenshots/KS.png" class="image"
-                                                title="KS.png" horizontalalign="center"
-                                                target="tutorial/screenshots/KS.png">
-                                                &nbsp&nbsp&nbsp&nbsp<img
-                                                src="tutorial/screenshots/KS.png" width="50%"
-                                                rel="popover" data-img="tutorial/screenshots/KS.png"
-                                                border="1px" align="center"> </a>
-                                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kathleen Steininger</p>
-                                                <p style="line-height:0.05"><i>&nbsp;&nbsp;&nbsp;&nbsp;Director of Operations</i></p>
-                                        </td>
-                                        <td style="width:200px"></td>
-                                        <td>
-                                        <BR> <BR> <a href="tutorial/screenshots/VG.jpg" class="image"
-                                                title="VG.jpg" horizontalalign="center"
-                                                target="tutorial/screenshots/VG.jpg">
-                                                &nbsp&nbsp&nbsp&nbsp<img
-                                                src="tutorial/screenshots/VG.jpg" width="50%"
-                                                rel="popover" data-img="tutorial/screenshots/VG.jpg"
-                                                border="1px" align="center"> </a>
-                                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Veronica Gutierrez</p>
-                                                <p style="line-height:0.05"><i>&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Event Manager</i></p>
-                                        </td>
-                                        <td style="width:100px"></td>
-                                        <td>
-                                        <BR> <BR> <a href="tutorial/screenshots/ER.jpg" class="image"
-                                                title="ER.jpg" horizontalalign="center"
-                                                target="tutorial/screenshots/ER.jpg">
-                                                &nbsp&nbsp&nbsp&nbsp<img
-                                                src="tutorial/screenshots/ER.jpg" width="50%"
-                                                rel="popover" data-img="tutorial/screenshots/ER.jpg"
-                                                border="1px" align="center"> </a>
-                                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;Emily Ripka</p>
-                                                <p style="line-height:0.05"><i>&nbsp;&nbsp;Marketing & PR Manager</i></p>
-                                        </td>
-                                    </tr>
-                                </table>
-				
-			</div>
-		<?PHP include('footer.inc');?>
-		</div>
-	</body>
+        <h3 class="section-title text-center">Our Team</h3>
+        <div class="team-row">
+            <div class="team-member">
+                <h4>Jennifer Griffin</h4>
+                <p>Co-founder & President</p>
+            </div>
+            <div class="team-member">
+                <h4>Joel Griffin</h4>
+                <p>Co-founder & Chairman</p>
+            </div>
+        </div>
+        <div class="team-row">
+            <div class="team-member">
+                <img src="tutorial/screenshots/KS.png" alt="Kathleen Steininger">
+                <h4>Kathleen Steininger</h4>
+                <p>Director of Operations</p>
+            </div>
+            <div class="team-member">
+                <img src="tutorial/screenshots/VG.jpg" alt="Veronica Gutierrez">
+                <h4>Veronica Gutierrez</h4>
+                <p>Event Manager</p>
+            </div>
+            <div class="team-member">
+                <img src="tutorial/screenshots/ER.jpg" alt="Emily Ripka">
+                <h4>Emily Ripka</h4>
+                <p>Marketing & PR Manager</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.inc'); ?>
+</body>
 </html>
