62acc99968b33f7979b48c500c708273997e7c61
ianwasem <ian.wasem@gmail.com>
2023-04-24 02:35:48 -0400

    Calendar Finished
    Fully finished calendar with times and fixed css issues.

--- calenderTemp.php
+++ calenderTemp.php
@@ -76,13 +76,13 @@ class Calender {
                 for ($d = 0; $d <= ($event[4]-1); $d++) {
                     if (date('y-m-d', strtotime($this->active_year . '-' . $this->active_month . '-' . $i . ' -' . $d . ' day')) == date('y-m-d', strtotime($event[1]))) {
                         if(strcmp(' green', $event[3])!=1){
-                            $html .= '<div class="event' . $event[0] . '">' . $event[2] .'-' . $event[3] . ' <a href=eventEdit.php?id='.$event[6].'>';
+                            $html .= '<div class="event' . $event[5] . '">' . $event[2] .'-' . $event[3] . ' <a href=eventEdit.php?id='.$event[6].'>';
                             $html .= $event[0];
                             $html .= '</a></div>
                                     ';
                         }
                         else{
-                            $html .= '<div class="event' . $event[0] . '"><a href=campaignEdit.php?id='.$event[6].'>';
+                            $html .= '<div class="event' . $event[5] . '"><a href=campaignEdit.php?id='.$event[6].'>';
                             $html .= $event[0];
                             $html .= '</a></div>
                                     ';
