eeb308c8ba159db1bb9543d10c435e9b6bfda7dd
jmundie5 <jmundie5@gmail.com>
2023-04-23 22:37:16 -0400

    Replaced Old About Page

--- about.php
+++ about.php
@@ -24,57 +24,33 @@ session_start();
 </head>
 <?php include('header.php'); ?>
 <body>
-<div class="container" style="padding-bottom: 100px;">
-    
+<div class="container" style="padding-bottom: 20px;">
+    <img class='mx-auto d-block'src="images\angelsIcon.png" alt="Angles on Wheels Icon" style='max-width: 20%'>
+          </a>
     <div id="content" class="mt-4">
-        <h2 class="text-center"><strong>About Gwyneth's Gift Foundation</strong></h2>
-        <p class="text-center">Gwyneth’s Gift Foundation is devoted to making a difference in the community and the lives of those within. We hope that through our work,
-            we can serve as a catalyst for increasing the survival rate of those suffering from an out-of-hospital cardiac arrest because they received
-            immediate life-saving measures from a member within their community.</p>
+        <h2 class="text-center"><strong>About Angels on Wheels</strong></h2>
+        <p class="text-center">The Angels are on a mission to “Love Thy Neighbors” by changing the lives of our neighbors who are at risk of becoming homeless and hungry. 
+            Providing resources and support such as food, shelter,  and necessities to improve the lives of our neighbors while bridging the gap between community 
+            agencies and their clients by providing volunteer services.</p>
 
-        <p class="text-center">Gwyneth’s Gift Foundation is a 501(c)(3) tax-exempt organization. Gwyneth’s Gift Foundation is Guidestar Gold Transparency, a member of
-            the Rappahannock United Way Local Government Campaign (LGC) and the Commonwealth of Virginia Campaign (CVC).</p>
+        <p class="text-center">Angels On Wheels Charity Organization (AOWCO) is a 501(c)(3) Nonprofit Organization. 
+            We were created to help those in need within the communities while working towards ending homelessness and hunger.</p>
 
-        <p class="text-center">Gwyneth’s Gift Foundation was founded in 2015 by Joel and Jennifer Griffin in honor of their oldest daughter, Gwyneth. Through the Foundation’s
-            work, Gwyneth’s spirit of caring, compassion, and community lives on as we create a world where everyone can save a life.</p>
+        <p class="text-center">The AOWCO helps people in need by addressing economic challenges such as issues that stem from low wages, unaffordable housing, and food insecurity. 
+            We provide a range of support and services to empower communities by addressing the root causes and social issues. 
+        We spend as much time and resources on homelessness and hunger prevention strategies and research as we provide services to those currently at risk of 
+        becoming homeless community, those suffering from food insecurities and those in need of resources.</p>
 
         <div>
-            <h3 class="section-title text-center">Our Vision</h3>
-            <p class="text-center">To create a Culture of Action where communities are educated, confident, and empowered to save the lives of individuals suffering from cardiac arrest.</p>
-        </div>
-        <div>
-            <h3 class="section-title text-center">Our Mission</h3>
-            <p class="text-center">To raise awareness of Cardiopulmonary Resuscitation (CPR) and the use of Automated External Defibrillators (AEDs).</p>
+            <h3 class="section-title text-center "><strong>Our Vision</strong></h3>
+            <p class="text-center">Our vision is a nation where every individual has access to the basic necessities of life and the opportunity to live with dignity and self-sufficient.
+A world without people in need,
+A community that collaborates so neighbors are helping other neighbors,
+A community where hope lives and hunger no longer forces acts of desperation,
+A thriving community where even the vulnerable neighbors are given the tools to flourish.</p>
         </div>
 
-        <h3 class="section-title text-center">Our Team</h3>
-        <div class="team-row">
-            <div class="team-member">
-                <h4>Jennifer Griffin</h4>
-                <p>Co-founder & President</p>
-            </div>
-            <div class="team-member">
-                <h4>Joel Griffin</h4>
-                <p>Co-founder & Chairman</p>
-            </div>
-        </div>
-        <div class="team-row">
-            <div class="team-member">
-                <img src="tutorial/screenshots/KS.png" alt="Kathleen Steininger">
-                <h4>Kathleen Steininger</h4>
-                <p>Director of Operations</p>
-            </div>
-            <div class="team-member">
-                <img src="tutorial/screenshots/VG.jpg" alt="Veronica Gutierrez">
-                <h4>Veronica Gutierrez</h4>
-                <p>Event Manager</p>
-            </div>
-            <div class="team-member">
-                <img src="tutorial/screenshots/ER.jpg" alt="Emily Ripka">
-                <h4>Emily Ripka</h4>
-                <p>Marketing & PR Manager</p>
-            </div>
-        </div>
+        
     </div>
 </div>
 <?php include('footer.php'); ?>
